938ea637e289cc0b6bf634da9b8bdc24f806c9e5
Oğuzhan Savaş <mustafaoguzhan.savas@bilgeadamboost.com>
2024-01-16 00:08:29 +0300

    Implemented interfaces adn classes for domain core

---
+++ MicroRabbit.Domain.Core/Events/Message.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Domain.Core.Events
+{
+    public abstract class Message : IRequest<bool>
+    {
+        public string MesageType { get;  protected set; }
+
+        protected Message()
+        {
+            MesageType = GetType().Name;
+        }
+    }
+}
